7e28d8dff8e1bd8ed9646c04ebb627e79e47fc93
YGYin <ygyin0307@gmail.com>
2024-05-31 20:17:08 +0800

    Basic project file

---
+++ src/main/java/com/ygyin/onlinejudgesandbox/OnlineJudgeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.ygyin.onlinejudgesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineJudgeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineJudgeSandboxApplication.class, args);
+    }
+
+}
